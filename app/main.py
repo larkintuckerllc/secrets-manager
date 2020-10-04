bcb6ad8fb08faff15d2468c62ea07866b0e621b0
John Tucker <john@larkintuckerllc.com>
2020-10-04 04:59:09 -0400

    stub in kubernetes

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from kubernetes import client, config
 
 
+config.load_kube_config()
+v1 = client.CoreV1Api()
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
+    ret = v1.list_pod_for_all_namespaces(watch=False)
+    for i in ret.items:
+        print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace,
+                              i.metadata.name))
     return 'Hello, World!'
