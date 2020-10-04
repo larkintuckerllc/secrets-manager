a74d35c58585f9be27a413f4d09de17d2b378072
John Tucker <john@larkintuckerllc.com>
2020-10-04 12:45:21 -0400

    version bump

--- app/__init__.py
+++ app/__init__.py
@@ -5,5 +5,5 @@ Compatible with tiangolo/meinheld-gunicorn:python3.8 Docker image.
 import os
 
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 development = os.environ.get('DEVELOPMENT') is not None
